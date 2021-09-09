98690506f48820f9d816aed71a8730568aef864d
terry <terrydavis903@gmail.com>
2021-09-08 22:59:55 -0400

    push

--- server.go
+++ server.go
@@ -4,13 +4,27 @@ import (
 	"context"
 	"github.com/ProjectAthenaa/sonic-core/sonic/core"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(compress.New())
+	app.Use(recover.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendFile("./scripts/index.html")
+		return c.SendFile("./index.html")
+	})
+
+	app.Get("/base.js", func(c *fiber.Ctx) error {
+		return c.SendFile("./base.js")
+	})
+
+	app.Get("/dynamic.js", func(c *fiber.Ctx) error {
+		return c.SendFile("./dynamic.js")
 	})
 
 	app.Post("/data", func(ctx *fiber.Ctx) error {
@@ -22,8 +36,6 @@ func main() {
 	app.Post("/submit", func(c *fiber.Ctx) error {
 		return c.SendString("nice job")
 	})
-
-	app.Static("/", "./scripts")
 
 	app.Listen(":8080")
 }
